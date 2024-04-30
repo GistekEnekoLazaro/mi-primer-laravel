ca91e2a24345877d3b61775a1fdf5ffdd66ca8d5
GistekEnekoLazaro <eneko.l@gistek-s.com>
2024-04-30 10:59:57 +0200

    Creada migracion y editados modelos
    Se ha creado una migracion que cree una tabla intermedia llamada idea_user en la que se han almacenado foreign keys y se han editado los modelos para poder hacer una relacion N:M

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Idea extends Model
 {
@@ -12,5 +14,10 @@ class Idea extends Model
     public function user(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function users(): belongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -48,5 +51,10 @@ class User extends Authenticatable
     public function idea(): HasMany
     {
         return $this->hasMany(Idea::class);
+    }
+
+    public function idealiked(): BelongsToMany
+    {
+        return $this->belongsToMany(Idea::class);
     }
 }
