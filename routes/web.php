7341373f5b47ea14f83b2980c19d75b285daad11
GistekEnekoLazaro <eneko.l@gistek-s.com>
2024-05-02 11:48:21 +0200

    Cambiadas algunas cosas

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index');
+    Route::get('/ideas/crear', [IdeaController::class, 'create'])->name('idea.create');
+    Route::post('/ideas/crear', [IdeaController::class, 'store'])->name('idea.store');
+    Route::get('/ideas/editar/{idea}', [IdeaController::class, 'edit'])->name('idea.edit');
+    Route::put('/ideas/actualizar/{idea}', [IdeaController::class, 'update'])->name('idea.update');
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show');
+    Route::delete('/ideas/{idea}', [IdeaController::class, 'delete'])->name('idea.delete');
+    Route::put('/ideas/{idea}', [IdeaController::class, 'syncronizeLikes'])->name('idea.delete');
+    Route::put('/ideas/{idea}', [IdeaController::class, 'syncronizeLikesIndex'])->name('idea.like');
+
 });
 
 require __DIR__.'/auth.php';
-
-
-Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index');
-Route::get('/ideas/crear', [IdeaController::class, 'create'])->name('idea.create');
-Route::post('/ideas/crear', [IdeaController::class, 'store'])->name('idea.store');
-Route::get('/ideas/editar/{idea}', [IdeaController::class, 'edit'])->name('idea.edit');
-Route::put('/ideas/actualizar/{idea}', [IdeaController::class, 'update'])->name('idea.update');
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show');
-Route::delete('/ideas/{idea}', [IdeaController::class, 'delete'])->name('idea.delete');
-Route::put('/ideas/{idea}', [IdeaController::class, 'syncronizeLikes'])->name('idea.delete');
-Route::put('/ideas/{idea}', [IdeaController::class, 'syncronizeLikesIndex'])->name('idea.like');
